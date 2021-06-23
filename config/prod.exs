0aa0ff0b1464b2296a44303b8932097245c9728b
Jonathan Silverman <jsilverman2@gmail.com>
2021-06-22 18:21:54 -0600

    prod

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :gbalbeat, GbalbeatWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  username: "postgres",
+  password: "postgres!!!442",
+  url: [host: "gbalbeat.gbaldraw.fun", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
