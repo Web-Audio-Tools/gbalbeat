d774fd20ebae652b35bf8e06e28dfa351a165582
Jonathan Silverman <jsilverman2@gmail.com>
2021-06-23 03:32:12 -0600

    just load the damn css

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -3,7 +3,7 @@ const glob = require('glob');
 const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+// const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => {
@@ -12,8 +12,7 @@ module.exports = (env, options) => {
   return {
     optimization: {
       minimizer: [
-        new TerserPlugin({ cache: true, parallel: true, sourceMap: devMode }),
-        new OptimizeCSSAssetsPlugin({})
+        new TerserPlugin({ cache: true, parallel: true, sourceMap: devMode })
       ]
     },
     entry: {
