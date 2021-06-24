5b59248ad4f63e738cd0cd0738e48c92f80c7a8d
Jonathan Silverman <jsilverman2@gmail.com>
2021-06-24 01:24:40 -0600

    starting point to add pd

--- assets/js/app.js
+++ assets/js/app.js
@@ -71,17 +71,16 @@ window.presence = presence;
 socket.connect();
 
 channel.on("new:msg", (msg) => {
-    if(msg["packets"] != undefined) {
+    if(msg["packets"] != undefined && 
+    msg["packets"]["action"] != undefined && 
+    msg["packets"]["args"] != undefined) {
         // reset daw
         for(var i = 0; i < UIhistoryActions.size; i++) DAW.history.undo();
         const packets = msg["packets"];
         packets.forEach(packet => {
             const p = JSON.parse(packet);
-            try{
-                DAW.callActionNoSend(p["action"], ...p["args"])
-            } catch(e) {
-                console.log("failed to apply action... " + e.message)
-            }
+
+            DAW.callActionNoSend(p["action"], ...p["args"])
 
             
         });
